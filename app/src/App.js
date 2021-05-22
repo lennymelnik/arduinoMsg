8f16d66f2bd23e44dc1b58421acfdc0e407459be
Leonard Melnik <leonardmelnik@gmail.com>
2021-05-22 17:51:59 -0400

    added fetch to react app

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
+async function sendMsg(msg) {
+  return fetch('http://192.168.0.237:8080/send-msg', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+     
+    },
+    body : JSON.stringify({msg : msg})
+  })
+    .then(data => data.json())
+ }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        
+        <input onChange={(e)=>{sendMsg(e.target.value)}}/>
+       
       </header>
     </div>
   );
